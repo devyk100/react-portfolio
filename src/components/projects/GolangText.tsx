149a4d037e76d9d7901a759ab1ee758da459b19c
devyk100 <devyk100@gmail.com>
2024-06-14 19:48:55 +0530

    added basic bento page

---
+++ src/components/projects/GolangText.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function GolangText() {
+  return <div className="p-2"></div>;
+}
+
+export default GolangText;
--- src/sections/ProjectSection.tsx
+++ src/sections/ProjectSection.tsx
@@ -1,5 +1,50 @@
+import { useMemo } from "react";
+import GolangText from "../components/projects/GolangText";
+
 function ProjectSection() {
-  return <section className="h-screen bg-black"></section>;
+  // const commonClass = useMemo(
+  //   () =>
+  //     " bg-indigo-200 rounded-md bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0 border border-gray-100",
+  //   []
+  // );
+  return (
+    <section className="h-screen bg-black">
+      <div className="grid grid-cols-3 px-[5.2vw] py-[4.2vh] grid-rows-3 h-screen w-screen gap-3">
+        <div
+          className={
+            "bg-black w-[30vw] h-[30vh] border-gray-500 border rounded-2xl"
+            // +
+            // commonClass
+          }
+        >
+          <GolangText />
+        </div>
+        <div
+          className={
+            "bg-black w-[60vw] h-[60vh] border-gray-500 border rounded-2xl col-span-2 row-span-2 min-w-full"
+            // +
+            // commonClass
+          }
+        ></div>
+        <div
+          className={
+            "bg-black w-[30vw] h-[30vh] border-gray-500 border rounded-2xl overflow-hidden "
+            // +
+            // commonClass
+          }
+        ></div>
+        <div
+          className={
+            "bg-black w-[90vw] h-[30vh] border-gray-500 border rounded-2xl row-span-3 min-w-full"
+            // +
+            // commonClass
+          }
+        >
+          f
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default ProjectSection;
